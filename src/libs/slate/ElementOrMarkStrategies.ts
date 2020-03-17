a0e191112600e0d263e9ffe6611c6e028bc2a390
André Alves <andre.alves@inspireit.pt>
2020-03-17 21:49:35 +0000

    :bug: Fix dependencies imports

--- src/libs/slate/ElementOrMarkStrategies.ts
+++ src/libs/slate/ElementOrMarkStrategies.ts
@@ -2,16 +2,12 @@ import { ElementOrMarkStrategyManager } from './ElementOrMarkStrategyManager';
 import { ReactEditor } from 'slate-react';
 
 import { BoldMark } from './Marks/BoldMark';
-import { CodeBlockElement } from './Elements/CodeBlockElement';
 import { ItalicMark } from './Marks/ItalicMark';
 import { UnderlineMark } from './Marks/UnderlineMark';
-import { TableElement } from './Elements/TableElement';
+
 import { TableHeaderElement } from './Elements/TableHeaderElement';
 import { TableHeaderCellElement } from './Elements/TableHeaderCellElement';
 import { TableBodyElement } from './Elements/TableBodyElement';
-import { TableRowElement } from './Elements/TableRowElement';
-import { TableCellElement } from './Elements/TableCellElement';
-import { ParagraphElement } from './Elements/ParagraphElement';
 
 import {
   TableElement,
@@ -27,14 +23,15 @@ export default (editor: ReactEditor): ElementOrMarkStrategyManager => {
     new BoldMark(editor),
     new ItalicMark(editor),
     new TableElement(editor),
-    new TableHeaderElement(editor),
-    new TableHeaderCellElement(editor),
-    new TableBodyElement(editor),
+    new UnderlineMark(editor),
     new TableRowElement(editor),
+    new TableBodyElement(editor),
     new TableCellElement(editor),
     new CodeBlockElement(editor),
     new ParagraphElement(editor),
-    new CodeHighlightElement(editor)
+    new TableHeaderElement(editor),
+    new CodeHighlightElement(editor),
+    new TableHeaderCellElement(editor)
   ];
 
   return new ElementOrMarkStrategyManager(strategies);
