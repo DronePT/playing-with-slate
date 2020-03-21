830ee81f7952b9729969ac10c21bf3e37cd06b60
FPJ Inspire <filipe.picoito@inspireit.pt>
2020-03-21 21:39:00 +0000

    ✨Add all the remaining buttons to the sub toolbar

--- src/components/Toolbar/components/SubToolbar/SubToolbar.tsx
+++ src/components/Toolbar/components/SubToolbar/SubToolbar.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Trash2 as TrashIcon } from 'react-feather';
+import {
+  ArrowRight as ArrowRightIcon,
+  ArrowLeft as ArrowLeftIcon,
+  ArrowDown as ArrowDownIcon,
+  ArrowUp as ArrowUpIcon,
+  Delete as DeleteIcon,
+  Trash2 as TrashIcon
+} from 'react-feather';
 
 import CustomEditor from '../../../../libs/slate/CustomEditor';
 
@@ -15,7 +22,29 @@ const SubToolbar: React.FC<SubToolbarProps> = props => {
 
   return (
     <div className="subtoolbar">
-      <button onClick={(): void => customEditor.sendKeyPress('meta+t+d')}>
+      <div className="subtoolbar__table__columns">
+        <button onClick={(): void => customEditor.sendKeyPress('meta+t+c+l')}>
+          <ArrowLeftIcon color="#333" size={24} />
+        </button>
+        <button onClick={(): void => customEditor.sendKeyPress('meta+t+c+r')}>
+          <ArrowRightIcon color="#333" size={24} />
+        </button>
+        <button onClick={(): void => customEditor.sendKeyPress('meta+t+c+del')}>
+          <DeleteIcon color="#333" size={24} />
+        </button>
+      </div>
+      <div className="subtoolbar__table__rows">
+        <button onClick={(): void => customEditor.sendKeyPress('meta+t+r+u')}>
+          <ArrowUpIcon color="#333" size={24} />
+        </button>
+        <button onClick={(): void => customEditor.sendKeyPress('meta+t+r+d')}>
+          <ArrowDownIcon color="#333" size={24} />
+        </button>
+        <button onClick={(): void => customEditor.sendKeyPress('meta+t+r+del')}>
+          <DeleteIcon color="#333" size={24} />
+        </button>
+      </div>
+      <button onClick={(): void => customEditor.sendKeyPress('meta+t+del')}>
         <TrashIcon color="#333" size={24} />
       </button>
     </div>
